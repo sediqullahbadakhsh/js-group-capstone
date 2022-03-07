a20391669d1cf4712ecd89d00c555c4e7a607e40
Nizamuddin Ahmadzai <78977308+Nizamuddin4493@users.noreply.github.com>
2022-03-08 01:36:18 +0430

    Webpack is completely setup

--- webpack.config.js
+++ webpack.config.js
@@ -9,11 +9,12 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './src/index.html',
+      title: 'Development',
+      template: 'src/index.html',
     }),
   ],
   output: {
-    filename: 'main.js',
+    filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
   },
@@ -22,6 +23,24 @@ module.exports = {
       {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.html$./,
+        use: ['html-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'images/',
+              publicPath: 'images/',
+            },
+          },
+        ],
+        type: 'asset/resource',
       },
     ],
   },
