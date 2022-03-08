4369d73526335ab00fccb40c474d9ac29b81472f
Nizamuddin Ahmadzai <78977308+Nizamuddin4493@users.noreply.github.com>
2022-03-08 21:29:02 +0430

    remove linter errors

--- src/modules/listItems.js
+++ src/modules/listItems.js
@@ -1,43 +1,43 @@
-//Fetching Data
+// Fetching Data
 const fetchData = async () => {
   const response = await fetch(
-    "https://www.themealdb.com/api/json/v1/1/filter.php?c=chicken"
+    'https://www.themealdb.com/api/json/v1/1/filter.php?c=chicken',
   );
   const data = await response.json();
   return data.meals;
 };
 
-//List Items
+// List Items
 const listItems = async () => {
-  const ul = document.querySelector(".recipes");
+  const ul = document.querySelector('.recipes');
   const food = await fetchData();
   food.forEach((element) => {
-    const li = document.createElement("li");
-    li.className = "recipe-item";
+    const li = document.createElement('li');
+    li.className = 'recipe-item';
     li.id = element.idMeal;
 
-    const img = document.createElement("img");
-    img.className = "food-img";
+    const img = document.createElement('img');
+    img.className = 'food-img';
     img.src = element.strMealThumb;
-    img.alt = "food";
+    img.alt = 'food';
 
-    const p = document.createElement("p");
+    const p = document.createElement('p');
     p.textContent = element.strMeal;
-    p.className = "recipe-name";
+    p.className = 'recipe-name';
 
-    const div = document.createElement("div");
-    div.className = "likes";
+    const div = document.createElement('div');
+    div.className = 'likes';
 
-    const heart = document.createElement("i");
-    heart.classList.add("far", "fa-heart");
+    const heart = document.createElement('i');
+    heart.classList.add('far', 'fa-heart');
     heart.id = element.idMeal;
-    const small = document.createElement("small");
-    small.textContent = "5 likes";
+    const small = document.createElement('small');
+    small.textContent = '5 likes';
 
-    const btn = document.createElement("button");
-    btn.type = "button";
-    btn.className = "comments";
-    btn.innerText = "Comments";
+    const btn = document.createElement('button');
+    btn.type = 'button';
+    btn.className = 'comments';
+    btn.innerText = 'Comments';
     btn.id = element.idMeal;
 
     div.appendChild(heart);
