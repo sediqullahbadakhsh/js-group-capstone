c10a63cba9914bbedb03bd94052ddfe8aa9192a8
sediqullahbadakhsh <sediqullah.badakhsh@gmail.com>
2022-03-10 10:17:43 +0430

    create comment and display comments

---
+++ src/modules/createComment.js
@@ -0,0 +1,54 @@
+import { postCommentAPI, getCommentsAPI } from './involvementAPI.js';
+
+const createComment = async (e, btn) => {
+  e.preventDefault();
+
+  const name = document.getElementById('name');
+  const message = document.getElementById('message');
+  const alert = document.getElementById('alert');
+  const itemC = document.getElementById('itemC');
+
+  if (name.value === '' || message.value === '') {
+    alert.textContent = 'Please fill all boxes';
+    setTimeout(() => {
+      alert.textContent = '';
+    }, 3000);
+    alert.style.color = 'red';
+    return;
+  }
+
+  const body = {
+    item_id: btn.id,
+    username: name.value,
+    comment: message.value,
+  };
+
+  const postComment = await postCommentAPI(body);
+  if (postComment === 'Created') {
+    message.textContent = 'Your comment was successfull Added';
+    setTimeout(() => {
+      alert.textContent = '';
+    }, 3000);
+    alert.style.color = 'green';
+  } else {
+    message.textContent = 'Something went wrong';
+    alert.style.color = 'red';
+    setTimeout(() => {
+      alert.textContent = '';
+    }, 3000);
+  }
+
+  name.value = '';
+  message.value = '';
+
+  const allComments = await getCommentsAPI(btn.id);
+
+  itemC.innerHTML = allComments
+    .map((e) => {
+      const { username, comment } = e;
+      return `<span>${e.creation_date} ${username}: ${comment}</span><br />`;
+    })
+    .join('<br />');
+};
+
+export default createComment;
