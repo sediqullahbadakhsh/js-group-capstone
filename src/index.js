dceba13bbdd390a8da4ecd5ce2dc00d2313daa51
sediqullahbadakhsh <sediqullah.badakhsh@gmail.com>
2022-03-11 12:37:25 +0430

    remove extra load function

--- src/index.js
+++ src/index.js
@@ -1,11 +1,7 @@
-import { listItems, listLikes } from './modules/listItems.js';
-import './style.css';
+import { listItems, listLikes } from "./modules/listItems.js";
+import "./style.css";
 
 window.onload = () => {
   listItems();
   listLikes();
 };
-
-window.addEventListener('load', () => {
-  listLikes();
-});
